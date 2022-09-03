884616f8a033c10706c753d4902e6b74a0bf1f5e
VSOFT\piotr.gamorski <piotr.famorski@vsoft.pl>
2022-09-03 17:15:32 +0100

    Timeline UI draft prepared and models created

---
+++ BlazorGrpcWebApp/Shared/Entities/LastActivity.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorGrpcWebApp.Shared.Entities
+{
+    public enum Activity 
+    {
+        Register,
+        Verify,
+        VerificationCodeReset,
+        PasswordReset,
+        Login,
+        BuildKnight,
+        BuildArcher,
+        BuildMage,
+        DeleteKnight,
+        DeleteArcher,
+        DeleteMage,
+        HealKnight,
+        HealArcher,
+        HealMage,
+        ReviveArmy,
+        WinAFight,
+        LostAFight
+    }
+    public class LastActivity
+    {
+        public int Id { get; set; }
+        public Activity ActivityType { get; set; }
+    }
+}
---
+++ BlazorGrpcWebApp/Shared/Entities/UserLastActivities.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorGrpcWebApp.Shared.Entities
+{
+    public class UserLastActivitie
+    {
+        public int Id { get; set; }
+        public DateTime ExecutionDate { get; set; } = DateTime.Now;
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public int LastActivityId { get; set; }
+        public virtual LastActivity LastActivity { get; set; }
+    }
+}
